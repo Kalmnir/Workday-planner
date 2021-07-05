2e1bfe0a5fb7f8b4360bcc3385b6aaf8a7dff54e
John-Mark Sackett <jmsackett91@gmail.com>
2021-07-05 11:05:09 -0400

    changed my box classes and got the save function working

--- java.js
+++ java.js
@@ -1,4 +1,38 @@
-var today = function () {
-    $(currentDay).text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
-};
-setInterval(today, 1000);
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+var time = moment();
+
+function setPlanner() {
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    }); setInterval(setPlanner, 1000);
+}
+
+setPlanner();
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".schedule").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+
+
+
