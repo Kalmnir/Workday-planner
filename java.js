8c15033c77f98d520d570bd7b08b3cb374c57620
John-Mark Sackett <jmsackett91@gmail.com>
2021-07-05 18:57:53 -0400

    some slight refactoring of my javascript and added a clear all button... because I wanted one

--- java.js
+++ java.js
@@ -1,20 +1,12 @@
-// declared global variables
-var hour1 = $("#9");
-var hour2 = $("#10");
-var hour3 = $("#11");
-var hour4 = $("#12");
-var hour5 = $("#13");
-var hour6 = $("#14");
-var hour7 = $("#15");
-var hour8 = $("#16");
-var hour9 = $("#17");
-var time = moment();
-
-// set planner function used to set the time and date on the website and set the text in each time block based on if there is anything saved in local storage
-function setPlanner() {
-
+// Sets the current time on the site
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+setInterval(function () {
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
+}, 1000);
+
+// set planner function used to set the text in each time block based on if there is anything saved in local storage
+function setPlanner() {
     $(".time-block").each(function () {
         var id = $(this).attr("id");
         var schedule = localStorage.getItem(id);
@@ -22,22 +14,32 @@ function setPlanner() {
         if (schedule !== null) {
             $(this).children(".schedule").val(schedule);
         }
-    }); setInterval(setPlanner, 1000);
+        else {
+            $(this).children(".schedule").val("");
+        }
+    });
 }
-
 setPlanner();
-var saveBtn = $(".saveBtn");
 
-// save button event listener and function
-saveBtn.on("click", function () {
+// save button and clear button event listener and function
+$(".saveBtn").on("click", function () {
     var time = $(this).parent().attr("id");
     var schedule = $(this).siblings(".schedule").val();
 
     localStorage.setItem(time, schedule);
 });
 
+$("#clear-btn").on("click", function () {
+    var confirmDelete = confirm("Are you sure you want to delete everything?")
+    if (confirmDelete) {
+        localStorage.clear()
+        setPlanner()
+    }
+})
+
 // function used to determine what the background color should be for each time block depending on time of day
 function pastPresentFuture() {
+    var time = moment();
     hour = time.hours();
     $(".time-block").each(function () {
         var thisHour = parseInt($(this).attr("id"));
@@ -53,5 +55,4 @@ function pastPresentFuture() {
         }
     })
 }
-
 pastPresentFuture();
